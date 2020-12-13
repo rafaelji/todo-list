4c21850e44c3e0dd9376400aff1e3bf9ad5e8714
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 04:08:54 -0300

    add route guard for app route

--- src/Router.js
+++ src/Router.js
@@ -3,14 +3,37 @@ import VueRouter from "vue-router";
 import Home from "@/pages/home/Home";
 import SignIn from "@/pages/sign-in/SignIn";
 import ToDoList from "@/pages/todo-list/ToDoList";
+import { auth } from "./services/api";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes: [
     { path: "/", component: Home },
     { path: "/sign-in", component: SignIn },
     { path: "/sign-up", component: SignIn },
-    { path: "/app", component: ToDoList },
+    {
+      path: "/app",
+      component: ToDoList,
+      meta: {
+        requiresAuth: true,
+      },
+    },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+
+  if (requiresAuth && !auth.currentUser) {
+    next("/sign-in");
+
+    return;
+  }
+
+  next();
+});
+
+export default router;
