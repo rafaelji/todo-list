0a13b0204f246dde8001132e6aa3825798568281
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 01:48:09 -0300

    change Navbar to use bootstrap components

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="center examplex">
-    <vs-navbar center-collapsed>
-      <template #right>
-        <vs-button @click="navigateToLoginPage" flat>Login</vs-button>
-      </template>
-    </vs-navbar>
-  </div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <router-link to="/" class="navbar-brand mr-0 mr-md-2">
+      To Do List
+    </router-link>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <router-link to="/login" class="nav-link">Login</router-link>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
 export default {
   name: "NavBar",
-  methods: {
-    navigateToLoginPage() {
-      this.$router.push("/login");
-    },
-  },
 };
 </script>
