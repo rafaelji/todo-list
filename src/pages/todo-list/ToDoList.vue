5fe410e38d990540981b616f8a1e3960b7d02a3f
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 02:54:42 -0300

    create scratch todo list item

--- src/pages/todo-list/ToDoList.vue
+++ src/pages/todo-list/ToDoList.vue
@@ -1,13 +1,40 @@
 <template>
-  <b-container class="bv-example-row">
-    <b-row>
-      <b-col>some content</b-col>
-    </b-row>
-  </b-container>
+  <div>
+    <NavBar />
+    <b-container fluid>
+      <b-row>
+        <b-col sm="12" md="4" class="d-flex justify-content-center">
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=26"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+          >
+            <b-card-text>
+              <ul>
+                <li>Fazer coisa 1</li>
+                <li>Fazer coisa 1</li>
+                <li>Fazer coisa 1</li>
+                <li>...</li>
+              </ul>
+            </b-card-text>
+
+            <b-button block href="#" variant="primary">Edit</b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/navbar/Navbar";
+
 export default {
   name: "ToDoList",
+  components: { NavBar },
 };
 </script>
