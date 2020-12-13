9f3cef6f32c4c8fd9c5d83cac1faac25796bf822
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-12 23:28:46 -0300

    add vue router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import Vuesax from 'vuesax'
 
 import 'vuesax/dist/vuesax.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
 Vue.use(Vuesax)
 
 new Vue({
