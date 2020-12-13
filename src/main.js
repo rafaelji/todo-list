1f1f6324e8c4da10e36874fc7f16aaf89e3b73a0
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 01:24:04 -0300

    remove vuesax library

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Vuesax from "vuesax";
 import router from "./Router";
 
-import "vuesax/dist/vuesax.css";
-
 Vue.config.productionTip = false;
-
-Vue.use(Vuesax);
 
 new Vue({
   router,
