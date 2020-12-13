a5dab74be7d4f65978a7148154bdf60820597dfd
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 05:09:09 -0300

    use functions to authenticate or create user

--- src/pages/sign-in/SignIn.vue
+++ src/pages/sign-in/SignIn.vue
@@ -5,7 +5,7 @@
       <b-row>
         <b-col offset-md="3" offset-lg="4" sm="12" md="6" lg="4">
           <b-card class="w-100">
-            <b-form>
+            <b-form @submit="onSubmit" @submit.prevent>
               <b-form-group
                 id="email-input-group"
                 label="Email address:"
@@ -16,6 +16,7 @@
                   type="email"
                   required
                   placeholder="Enter email"
+                  v-model.trim="email"
                 ></b-form-input>
               </b-form-group>
 
@@ -29,10 +30,14 @@
                   type="password"
                   required
                   placeholder="Enter password"
+                  v-model.trim="password"
                 ></b-form-input>
               </b-form-group>
 
-              <b-button block type="submit" variant="primary">Login</b-button>
+              <b-button block type="submit" variant="primary">
+                <b-spinner v-if="loading" small label="Spinning"></b-spinner>
+                {{ accessType }}
+              </b-button>
             </b-form>
           </b-card>
         </b-col>
@@ -43,11 +48,35 @@
 
 <script>
 import NavBar from "@/components/navbar/Navbar";
+import UserService from "@/services/UserService";
 
 export default {
   name: "SignIn",
   components: {
     NavBar,
+  },
+  data: () => ({ accessType: "", email: "", password: "", loading: false }),
+  created: function () {
+    this.accessType = this.$route.path === "/sign-in" ? "Sign In" : "Sign Up";
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        this.loading = true;
+
+        if (this.accessType === "Sign In") {
+          await UserService.signIn(this.email, this.password);
+          await this.$router.push("/app");
+
+          return;
+        }
+
+        await UserService.signUp(this.email, this.password);
+        await this.$router.push("/app");
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
