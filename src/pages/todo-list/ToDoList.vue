74eb95d50f03791b0ce6f71aa1545fcfc15bac30
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 02:41:34 -0300

    create todo list component

---
+++ src/pages/todo-list/ToDoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col>some content</b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "ToDoList",
+};
+</script>
