9135823345cc1ede43627b930c591a2cbb7625b5
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 02:33:24 -0300

    create sign-in component

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/pages/home/Home";
-import Login from "@/pages/login/Login";
+import SignIn from "@/pages/sign-in/SignIn";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
     { path: "/", component: Home },
-    { path: "/login", component: Login },
+    { path: "/sign-in", component: SignIn },
   ],
 });
---
+++ src/pages/sign-in/SignIn.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <NavBar />
+    <b-container fluid>
+      <b-row>
+        <b-col offset-md="3" offset-lg="4" sm="12" md="6" lg="4">
+          <b-card class="w-100">
+            <b-form>
+              <b-form-group
+                id="email-input-group"
+                label="Email address:"
+                label-for="email"
+              >
+                <b-form-input
+                  id="email"
+                  type="email"
+                  required
+                  placeholder="Enter email"
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                id="password-input-group"
+                label="Password:"
+                label-for="password"
+              >
+                <b-form-input
+                  id="password"
+                  type="password"
+                  required
+                  placeholder="Enter password"
+                ></b-form-input>
+              </b-form-group>
+
+              <b-button block type="submit" variant="primary">Login</b-button>
+            </b-form>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/navbar/Navbar";
+
+export default {
+  name: "SignIn",
+  components: {
+    NavBar,
+  },
+};
+</script>
