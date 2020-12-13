7b7f1b0b273915143f434e9fe034b7ac96ab38c5
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-13 06:44:07 -0300

    create notification component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <Notification />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import Notification from "@/components/notification/Notification";
 export default {
   name: "App",
+  components: { Notification },
 };
 </script>
---
+++ src/components/notification/Notification.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <b-toast id="toast" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notification",
+  methods: {
+    showErrorToast() {
+      this.$bvToast.toast(
+        `A error happened, run to the hills! Or you can look at the browser console and try to figure out the problem ;)`,
+        {
+          toaster: "b-toaster-bottom-center",
+          variant: "danger",
+          title: "Attention",
+          autoHideDelay: 3000,
+        }
+      );
+    },
+    showSuccessToast(message) {
+      this.$bvToast.toast(message, {
+        toaster: "b-toaster-bottom-center",
+        variant: "success",
+        title: "Success",
+        autoHideDelay: 3000,
+      });
+    },
+    showInfoToast(title, message) {
+      this.$bvToast.toast(message, {
+        toaster: "b-toaster-bottom-center",
+        variant: "info",
+        title: title,
+        autoHideDelay: 5000,
+      });
+    },
+  },
+  computed: {
+    notification() {
+      return this.$store.getters.notification;
+    },
+  },
+  watch: {
+    notification(newState) {
+      if (newState.type === "error") {
+        this.showErrorToast();
+      }
+
+      if (newState.type === "success") {
+        this.showSuccessToast(newState.message);
+      }
+
+      if (newState.type === "info") {
+        this.showInfoToast(newState.title, newState.message);
+      }
+    },
+  },
+};
+</script>
