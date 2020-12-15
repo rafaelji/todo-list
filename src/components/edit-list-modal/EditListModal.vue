097c05342eca0bc856f68027d52e27bcf9d29bba
Rafael Barbosa Silva <r.ji@outlook.com>
2020-12-15 01:55:23 -0300

    create modal component to edit to do list

---
+++ src/components/edit-list-modal/EditListModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-button
+      id="show-btn"
+      @click="$bvModal.show(`bv-modal-${currentToDoList.id}`)"
+      >Open Modal</b-button
+    >
+
+    <b-modal :id="`bv-modal-${currentToDoList.id}`" hide-footer>
+      <template #modal-title>{{ currentToDoList.title }}</template>
+      <div class="d-block text-center">
+        <h3>Hello From This Modal!</h3>
+      </div>
+      <b-button
+        class="mt-3"
+        block
+        @click="$bvModal.hide(`bv-modal-${currentToDoList.id}`)"
+        >Close Me</b-button
+      >
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  name: "EditListModal",
+  props: {
+    id: Number,
+    title: String,
+    imgUrl: String,
+    items: Array,
+  },
+  computed: {
+    currentToDoList() {
+      return this.$store.getters.currentToDoList[
+        this.$store.getters.currentToDoList.findIndex(
+          (item) => item.id === this.id
+        )
+      ];
+    },
+  },
+};
+</script>
--- src/components/to-do-list-item/ToDoListItem.vue
+++ src/components/to-do-list-item/ToDoListItem.vue
@@ -1,31 +1,37 @@
 <template>
-  <b-card
-    :img-src="imgUrl"
-    img-alt="Image"
-    img-top
-    tag="div"
-    style="max-width: 20rem"
-    class="mb-2"
-  >
-    <b-card-text>
-      <h1>{{ title }}</h1>
-      <ul>
-        <li v-for="item in items" :key="item.id">{{ item.value }}</li>
-      </ul>
-    </b-card-text>
+  <div>
+    <EditListModal :id="id" :title="title" />
+    <b-card
+      :img-src="imgUrl"
+      img-alt="Image"
+      img-top
+      tag="div"
+      style="max-width: 20rem"
+      class="mb-2"
+    >
+      <b-card-text>
+        <h1>{{ title }}</h1>
+        <ul>
+          <li v-for="item in items" :key="item.id">{{ item.value }}</li>
+        </ul>
+      </b-card-text>
 
-    <b-button variant="primary" class="float-left">
-      <b-icon icon="pencil" aria-hidden="true" />
-    </b-button>
-    <b-button @click="removeList" variant="danger" class="float-right">
-      <b-icon icon="trash" aria-hidden="true" />
-    </b-button>
-  </b-card>
+      <b-button variant="primary" class="float-left">
+        <b-icon icon="pencil" aria-hidden="true" />
+      </b-button>
+      <b-button @click="removeList" variant="danger" class="float-right">
+        <b-icon icon="trash" aria-hidden="true" />
+      </b-button>
+    </b-card>
+  </div>
 </template>
 
 <script>
+import EditListModal from "@/components/edit-list-modal/EditListModal";
+
 export default {
   name: "ToDoListItem",
+  components: { EditListModal },
   props: {
     id: Number,
     title: String,
